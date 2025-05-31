f3ed53043f629828687c7173b002463c564901d1
sonwjnn <hoangsonj4f@gmail.com>
2025-05-31 16:22:37 +0700

    build responsive for categories ui

--- src/app/(app)/(home)/search-filter/subcategory-menu.tsx
+++ src/app/(app)/(home)/search-filter/subcategory-menu.tsx
@@ -1,8 +1,8 @@
-import { Category } from "@/payload-types";
 import Link from "next/link";
+import { CustomCategory } from "../types";
 
 interface Props {
-  category: Category;
+  category: CustomCategory;
   isOpen: boolean;
   position: { top: number; left: number };
 }
@@ -23,10 +23,10 @@ export const SubcategoryMenu = ({ category, isOpen, position }: Props) => {
         className="w-60  text-black rounded-md overflow-hidden border shadow-[4px_4px_0px_0px_rgba(0,0,0,1)] -translate-x-[2px] -translate-y-[2px]"
       >
         <div>
-          {category.subcategories.map((subcategory: Category) => (
+          {category.subcategories.map((subcategory) => (
             <Link
               key={subcategory.id}
-              href={`/`}
+              href={`/${category.slug}/${subcategory.slug}`}
               className="w-full text-left p-4 hover:bg-black hover:text-white flex justify-between items-center underline font-medium"
             >
               {subcategory.name}
