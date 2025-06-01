41683497d36c84cb01415f7649262fd22cdde1e4
sonwjnn <hoangsonj4f@gmail.com>
2025-06-02 00:59:52 +0700

    build filters sorting feature

--- src/modules/products/ui/components/product-filters.tsx
+++ src/modules/products/ui/components/product-filters.tsx
@@ -5,6 +5,7 @@ import { ChevronDownIcon, ChevronRightIcon } from "lucide-react";
 import { useState } from "react";
 import { PriceFilter } from "./price-filter";
 import { useProductFilters } from "../../hooks/use-product-filter";
+import { TagsFilter } from "./tags-filter";
 
 interface Props {
   title: string;
@@ -33,29 +34,62 @@ const ProductFilter = ({ title, className, children }: Props) => {
 export const ProductFilters = () => {
   const [filters, setFilters] = useProductFilters();
 
+  const hasAnyFilters = Object.entries(filters).some(([key, value]) => {
+    if (key === "sort") {
+      return false;
+    }
+
+    if (Array.isArray(value)) {
+      return value.length > 0;
+    }
+
+    if (typeof value === "string") {
+      return value !== "";
+    }
+    return value !== null;
+  });
+
   const onChange = (key: keyof typeof filters, value: unknown) => {
-    return (value: string) => {
-      setFilters({
-        ...filters,
-        [key]: value,
-      });
-    };
+    return setFilters({
+      ...filters,
+      [key]: value,
+    });
+  };
+
+  const onClear = () => {
+    setFilters({
+      minPrice: "",
+      maxPrice: "",
+      tags: [],
+    });
   };
 
   return (
     <div className="border rounded-md bg-white">
       <div className="p-4 border-b flex items-center justify-between">
         <p className="font-medium">Filters</p>
-        <button className="underline" onClick={() => {}} type="button">
-          Clear
-        </button>
+        {hasAnyFilters && (
+          <button
+            className="underline cursor-pointer"
+            onClick={onClear}
+            type="button"
+          >
+            Clear
+          </button>
+        )}
       </div>
-      <ProductFilter title="Price" className="border-b-0">
+      <ProductFilter title="Price">
         <PriceFilter
           minPrice={filters.minPrice}
           maxPrice={filters.maxPrice}
           onMinPriceChange={(value) => onChange("minPrice", value)}
           onMaxPriceChange={(value) => onChange("maxPrice", value)}
+        />
+      </ProductFilter>
+      <ProductFilter title="Tags" className="border-b-0">
+        <TagsFilter
+          tags={filters.tags}
+          onTagsChange={(value) => onChange("tags", value)}
         />
       </ProductFilter>
     </div>
---
+++ src/modules/products/ui/components/tags-filter.tsx
@@ -0,0 +1,71 @@
+import { Checkbox } from "@/components/ui/checkbox";
+import { DEFAULT_LIMIT } from "@/constants";
+import { useTRPC } from "@/trpc/client";
+import { useInfiniteQuery } from "@tanstack/react-query";
+import { LoaderIcon } from "lucide-react";
+
+interface TagsFilterProps {
+  tags?: string[] | null;
+  onTagsChange: (tags: string[]) => void;
+}
+export const TagsFilter = ({ tags, onTagsChange }: TagsFilterProps) => {
+  const trpc = useTRPC();
+  const { data, isLoading, fetchNextPage, hasNextPage, isFetchingNextPage } =
+    useInfiniteQuery(
+      trpc.tags.getMany.infiniteQueryOptions(
+        {
+          limit: DEFAULT_LIMIT,
+        },
+        {
+          getNextPageParam: (lastPage) => {
+            return lastPage.docs.length > 0 ? lastPage.nextPage : undefined;
+          },
+        }
+      )
+    );
+
+  const onClick = (tag: string) => {
+    if (tags?.includes(tag)) {
+      onTagsChange(tags.filter((t) => t !== tag));
+    } else {
+      onTagsChange([...(tags || []), tag]);
+    }
+  };
+
+  return (
+    <div className="flex flex-col gap-y-2">
+      {isLoading ? (
+        <div className="flex items-center justify-center p-4">
+          <LoaderIcon className="size-4 animate-spin" />
+        </div>
+      ) : (
+        data?.pages.map((page) =>
+          page.docs.map((tag) => (
+            <div
+              key={tag.id}
+              className="flex items-center justify-between cursor-pointer"
+              onClick={() => onClick(tag.name)}
+            >
+              <p className="font-medium">{tag.name}</p>
+              <Checkbox
+                checked={tags?.includes(tag.name)}
+                onCheckedChange={() => onClick(tag.name)}
+              />
+            </div>
+          ))
+        )
+      )}
+
+      {hasNextPage && (
+        <button
+          type="button"
+          onClick={() => fetchNextPage()}
+          disabled={isFetchingNextPage}
+          className="underline font-medium justify-start text-start disabled:opacity-50 cursor-pointer"
+        >
+          Load more...
+        </button>
+      )}
+    </div>
+  );
+};
